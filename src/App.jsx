7f2c54db662c36b97cce63ae2c3cfac3c42fc4d5
deepak singh <deepaksingh101101@gmail.com>
2024-04-30 12:22:08 +0530

    Course Page integrated with id in params

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/course" element={<Course />}/>
+        <Route path="/course/:id" element={<Course />}/>
         
       </Routes>
     </BrowserRouter>
